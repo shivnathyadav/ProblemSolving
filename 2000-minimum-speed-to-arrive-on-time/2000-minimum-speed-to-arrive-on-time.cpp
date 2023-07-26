55c06d35b8500ad571d2451479031cc35b81536e
shivnathyadav <95693049+shivnathyadav@users.noreply.github.com>
2023-07-26 17:34:10 +0530

    Time: 195 ms (100%), Space: 101.4 MB (94.1%) - LeetHub

---
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool valid(vector<int>& dist,double hour,long speed){
+        double ans = 0;
+        for(int i = 0 ; i < dist.size() ; i++){
+            ans = ceil(ans);
+            ans += (double)dist[i]/speed;
+            if(ans > hour){
+                return false;
+            }
+        }
+        return true;
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        int n =  dist.size();
+        if(hour < n-1)
+            return -1;
+        long miniSpeed = 1 , maxiSpeed = 1e7;
+        while(miniSpeed <= maxiSpeed){
+            long speed = (miniSpeed + maxiSpeed)/2;
+         //   cout<<maxiSpeed<<" "<<miniSpeed<<" "<<speed<<endl;
+         //   cout<<valid(dist,hour,speed)<<endl;
+            if(valid(dist,hour,speed)){
+                
+                maxiSpeed = speed - 1;
+            }
+            else{
+                miniSpeed = speed + 1;
+            }
+
+        }
+        cout<<maxiSpeed<<" "<<miniSpeed<<endl;
+        if(miniSpeed>1e7){
+            return -1;
+        }
+        return miniSpeed;
+    }
+};
