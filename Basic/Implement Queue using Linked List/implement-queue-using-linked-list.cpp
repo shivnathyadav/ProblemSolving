c10d60fcf8e50e2e77f116af802cac4068c8e786
Shiv Nath Yadav <95693049+shivnathyadav@users.noreply.github.com>
2023-08-02 23:53:27 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Implement Queue using Linked List/implement-queue-using-linked-list.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+};
+
+
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+
+// } Driver Code Ends
+
+
+/* Structure of a node in Queue
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+    QueueNode(int a)
+    {
+        data = a;
+        next = NULL;
+    }
+};
+
+And structure of MyQueue
+struct MyQueue {
+    QueueNode *front;
+    QueueNode *rear;
+    void push(int);
+    int pop();
+    MyQueue() {front = rear = NULL;}
+}; */
+
+//Function to push an element into the queue.
+void MyQueue:: push(int x)
+{
+        // Your Code
+        
+        QueueNode* insert = new QueueNode(x);
+        if(front==NULL){
+            front=rear=insert;
+        }
+        else{
+            rear->next = insert;
+            rear = rear ->next;
+        }
+        
+}
+
+//Function to pop front element from the queue.
+int MyQueue :: pop()
+{
+        // Your Code   
+        if(front == NULL){
+            return -1;
+        }
+        else if(front==rear){
+            int val = front -> data;
+            front=rear=NULL;
+            return val;
+        }
+        else{
+            int ans = front -> data;
+        
+            front = front->next;
+            return ans;    
+        }
+        
+}
