0ad7d144dc7564cc23e6eadd6be3894a9cb0b3af
Shiv Nath Yadav <95693049+shivnathyadav@users.noreply.github.com>
2023-08-09 19:49:18 +0530

    Time: 11 ms (99.05%), Space: 31.1 MB (78.23%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n = nums.size();
+        int rightSum = accumulate(nums.begin(),nums.end(),0);
+        int leftSum = 0;
+        for(int i = 0; i < n; i++){
+            rightSum -= nums[i];
+            if(rightSum == leftSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+};
