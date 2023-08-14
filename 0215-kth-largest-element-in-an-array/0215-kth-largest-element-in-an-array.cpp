9f5e6581225db0241862004393b7c74217b9a445
Shiv Nath Yadav <95693049+shivnathyadav@users.noreply.github.com>
2023-08-14 14:33:58 +0530

    Time: 45 ms (99.91%), Space: 45.6 MB (74.16%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        int n = nums.size();
-        map<int,int> mp;
-        for(auto it :  nums){
-            mp[it]++;
-        }
-        k = n - k + 1;
-        for(auto it: mp){
-            cout<<k<<endl;
-            if(k-it.second>0){
-                k = k - it.second;
-            }
-            else{
-                return it.first;
-            }
-        }
-        return -1;
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        // put (n-k)th element at is right position and all element which are smaller than sorted(n-k)th before it and larger are should be after it.
+        nth_element(nums.begin(), nums.end() - k, nums.end());
+
+        return nums[nums.size()-k];
     }
 };
