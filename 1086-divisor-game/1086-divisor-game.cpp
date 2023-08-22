051b6af0547c8b424672522a5ee698268113e609
Shiv Nath Yadav <95693049+shivnathyadav@users.noreply.github.com>
2023-08-22 23:46:23 +0530

    Time: 0 ms (100%), Space: 5.8 MB (97.89%) - LeetHub

--- 1086-divisor-game/1086-divisor-game.cpp
+++ 1086-divisor-game/1086-divisor-game.cpp
@@ -1,53 +1,14 @@
 class Solution {
 public:
-    vector<int> checkDivisor(int n){
-        vector<int> vec;
-        vec.push_back(1);
-        for(int i = 2; i*i <= n; i++){
-            if(n%i == 0){
-                vec.push_back(i);
-                if(i!=(n/i)){
-                    vec.push_back(n/i);
-                }
-            }
-        }
-        return vec;
-    }
-    bool game(int n,int ch){
-        if(n <= 1 && ch == 0){
-            return false;
-        }
-        else if(n <= 1 && ch == 1){
-            return true;
-        } 
-        else if(n==2 && ch == 0){
-            return true;
-        }
-        else if(n==2 && ch == 1){
-            return false;
-        } 
-        vector<int> divisor = checkDivisor(n);
-        for(int  i = 0; i < divisor.size(); i++){
-            if(ch == 0){
-                if(game(n-divisor[i],1)){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                if(game(n-divisor[i],0)){
-                    return true;
-                }
-                else{
-                    return false;
-                }
-            }    
-        }
-        return false;
-    }
     bool divisorGame(int n) {
-        return game(n,0);
+        bool dp[n+1];
+        memset(dp, false, n+1);
+        for(int i=2; i <= n; i++)
+            for(int j = 1; j*j <= i;j++){
+                if(i % j == 0 && !dp[i - j])
+                    dp[i] = true;
+            }
+        
+        return dp[n];
     }
 };
