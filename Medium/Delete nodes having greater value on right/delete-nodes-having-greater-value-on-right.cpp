080a80a0e2614adf93459a30897c5839e5391b8f
Shiv Nath Yadav <95693049+shivnathyadav@users.noreply.github.com>
2023-08-29 08:17:23 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Delete nodes having greater value on right/delete-nodes-having-greater-value-on-right.cpp
@@ -0,0 +1,127 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+// } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*/
+class Solution
+{
+    public:
+    Node* reverse(Node* head)
+    {
+        // Initialize current, previous and next pointers
+        Node* current = head;
+        Node *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            // Store next
+            next = current->next;
+            // Reverse current node's pointer
+            current->next = prev;
+            // Move pointers one position ahead.
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        head = reverse(head);
+        int maxi = head->data;
+        Node* temp = head;
+        Node* prev = head;
+        temp = temp->next;
+        while(temp){
+            if(temp->data<maxi){
+                temp = temp->next;
+            }
+            else{
+                maxi = max(maxi,temp -> data);
+                prev ->next = temp;
+                prev = prev -> next;
+                temp = temp ->next;
+                
+            }
+        }
+        prev ->next = NULL;
+        head = reverse(head);
+        return head;
+    }
+    
+};
+   
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+
+// } Driver Code Ends
