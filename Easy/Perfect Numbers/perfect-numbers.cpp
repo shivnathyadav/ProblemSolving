4c305ed0eaffc15b0b1238beca50d3269a87c5df
Shiv Nath Yadav <95693049+shivnathyadav@users.noreply.github.com>
2023-09-12 08:59:11 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Perfect Numbers/perfect-numbers.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int isPerfectNumber(long long N) {
+        // code here
+        long long pns[] = {6l,28l,496l,8128l,33550336l,8589869056l,137438691328l};
+        for (long long pn : pns) {
+            if (pn == N) return 1;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.isPerfectNumber(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
